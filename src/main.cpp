40fa6658f581936120c7321b5707ff7d2e38ca4f
darkvictor13 <01victoralmeida@gmail.com>
2022-02-15 17:21:37 -0300

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -27,88 +27,86 @@ int main(int argc, char** argv)
     cv::Mat AINDANE_dst;
     AINDANE(src, AINDANE_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("AINDANE处理时间: ", start_time, end_time);
+    MyTimeOutput("AINDANE: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat WTHE_dst;
     WTHE(src, WTHE_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("WTHE处理时间: ", start_time, end_time);
+    MyTimeOutput("WTHE: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat GCEHistMod_dst;
     GCEHistMod(src, GCEHistMod_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("GCEHistMod处理时间: ", start_time, end_time);
+    MyTimeOutput("GCEHistMod: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat LDR_dst;
     LDR(src, LDR_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("LDR处理时间: ", start_time, end_time);
+    MyTimeOutput("LDR: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat AGCWD_dst;
     AGCWD(src, AGCWD_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("AGCWD处理时间: ", start_time, end_time);
+    MyTimeOutput("AGCWD: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat AGCIE_dst;
     AGCIE(src, AGCIE_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("AGCIE处理时间: ", start_time, end_time);
+    MyTimeOutput("AGCIE: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat IAGCWD_dst;
     IAGCWD(src, IAGCWD_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("IAGCWD处理时间: ", start_time, end_time);
+    MyTimeOutput("IAGCWD: ", start_time, end_time);
 
-    start_time = high_resolution_clock::now();
-    cv::Mat Ying_dst;
-    Ying_2017_CAIP(src, Ying_dst);
-    end_time = high_resolution_clock::now();
-    MyTimeOutput("Ying处理时间: ", start_time, end_time);
+    //start_time = high_resolution_clock::now();
+    //cv::Mat Ying_dst;
+    //Ying_2017_CAIP(src, Ying_dst);
+    //end_time = high_resolution_clock::now();
+    //MyTimeOutput("Ying处理时间: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat CEusingLuminanceAdaptation_dst;
     CEusingLuminanceAdaptation(src, CEusingLuminanceAdaptation_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("CEusingLuminanceAdaptation处理时间: ", start_time, end_time);
+    MyTimeOutput("CEusingLuminanceAdaptation: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat adaptiveImageEnhancement_dst;
     adaptiveImageEnhancement(src, adaptiveImageEnhancement_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("adaptiveImageEnhancement处理时间: ", start_time, end_time);
-    
+    MyTimeOutput("adaptiveImageEnhancement: ", start_time, end_time);
+
     start_time = high_resolution_clock::now();
     cv::Mat JHE_dst;
     JHE(src, JHE_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("JHE处理时间: ", start_time, end_time);
+    MyTimeOutput("JHE: ", start_time, end_time);
 
     start_time = high_resolution_clock::now();
     cv::Mat SEF_dst;
     SEF(src, SEF_dst);
     end_time = high_resolution_clock::now();
-    MyTimeOutput("SEF处理时间: ", start_time, end_time);
+    MyTimeOutput("SEF: ", start_time, end_time);
 
-    cv::imshow("src", src);
-    cv::imshow("AINDANE_dst", AINDANE_dst);
-    cv::imshow("WTHE_dst", WTHE_dst);
-    cv::imshow("GCEHistMod_dst", GCEHistMod_dst);
-    cv::imshow("LDR_dst", LDR_dst);
-    cv::imshow("AGCWD_dst", AGCWD_dst);
-    cv::imshow("AGCIE_dst", AGCIE_dst);
-    cv::imshow("IAGCWD_dst", IAGCWD_dst);
-    cv::imshow("Ying_dst", Ying_dst);
-    cv::imshow("CEusingLuminanceAdaptation_dst", CEusingLuminanceAdaptation_dst);
-    cv::imshow("adaptiveImageEnhancement_dst", adaptiveImageEnhancement_dst);
-    cv::imshow("JHE_dst", JHE_dst);
-    cv::imshow("SEF_dst", SEF_dst);
-	
-    cv::waitKey();
+    cv::imwrite("images/01_src.bmp", src);
+    cv::imwrite("images/02_AINDANE_dst.bmp", AINDANE_dst);
+    cv::imwrite("images/03_WTHE_dst.bmp", WTHE_dst);
+    cv::imwrite("images/04_GCEHistMod_dst.bmp", GCEHistMod_dst);
+    cv::imwrite("images/05_LDR_dst.bmp", LDR_dst);
+    cv::imwrite("images/06_AGCWD_dst.bmp", AGCWD_dst);
+    cv::imwrite("images/07_AGCIE_dst.bmp", AGCIE_dst);
+    cv::imwrite("images/08_IAGCWD_dst.bmp", IAGCWD_dst);
+    cv::imwrite("images/09_CEusingLuminanceAdaptation_dst.bmp", CEusingLuminanceAdaptation_dst);
+    cv::imwrite("images/10_adaptiveImageEnhancement_dst.bmp", adaptiveImageEnhancement_dst);
+    cv::imwrite("images/11_JHE_dst.bmp", JHE_dst);
+    cv::imwrite("images/12_SEF_dst.bmp", SEF_dst);
+
     return 0;
 }
